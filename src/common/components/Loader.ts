43b8ece2696196ef4bea3339798a481dd56e0f93
michal-lepik <michal.lepik@hotmail.com>
2020-09-04 04:47:33 +0200

    Add loader component

---
+++ src/common/components/Loader.ts
@@ -0,0 +1,29 @@
+import styled, { keyframes } from 'styled-components';
+
+const rotate360 = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+interface Props {
+    small?: boolean;
+}
+
+export const Loader = styled.div`
+    margin: 10rem auto;
+    animation: ${rotate360} 1s linear infinite;
+    transform: translateZ(0);
+
+    border-top: 2px solid grey;
+    border-right: 2px solid grey;
+    border-bottom: 2px solid grey;
+    border-left: 4px solid black;
+    background: transparent;
+    width: ${(props: Props) => (props.small ? '30px' : '100px')};
+    height: ${(props: Props) => (props.small ? '30px' : '100px')};
+    border-radius: 50%;
+`;
